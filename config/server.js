285baa4c27428248ca2d255ca756cee4d668bc07
lorraineliang <345218209@qq.com>
2021-04-18 00:44:15 +0800

    fix:端口号

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '7a6a8dbd6282f3e70ca8106ebbbf890c'),
